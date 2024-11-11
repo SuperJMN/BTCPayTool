dbdb2eb156d42d9d384293dcc8e1cc2e2c89c830
José Manuel Nieto <superjmn@outlook.com>
2024-11-11 10:35:46 +0100

    Remove comments

--- BTCPayTool/Program.cs
+++ BTCPayTool/Program.cs
@@ -12,10 +12,8 @@ namespace BTCPayTool
                 .WriteTo.Console()
                 .CreateLogger();
 
-            // Define el comando raíz y agrega descripciones generales
             var rootCommand = new RootCommand("BTCPayTool CLI tool");
 
-            // Comando para crear un nuevo plugin
             var newPluginCommand = new Command("new-plugin", "Creates a new plugin");
             var nameOption = new Option<string>("--name", "The name of the plugin") { IsRequired = true };
             newPluginCommand.AddOption(nameOption);
@@ -25,7 +23,6 @@ namespace BTCPayTool
                 await NewPlugin(new NewPluginOptions { Name = name });
             }, nameOption);
 
-            // Comando para inicializar una solución de plugins
             var initPluginCommand = new Command("init-plugin", "Initializes a new plugin solution");
             var solutionNameOption = new Option<string>("--name", "The name of the solution") { IsRequired = true };
             initPluginCommand.AddOption(solutionNameOption);
@@ -35,15 +32,13 @@ namespace BTCPayTool
                 await InitializePluginSolution(new InitializePluginSolutionOptions { Name = name });
             }, solutionNameOption);
 
-            // Agrega los comandos al comando raíz
             rootCommand.AddCommand(newPluginCommand);
             rootCommand.AddCommand(initPluginCommand);
 
-            // Ejecuta el comando raíz
             return await rootCommand.InvokeAsync(args);
         }
 
-        private static async Task<int> InitializePluginSolution(InitializePluginSolutionOptions opts)
+        private static async Task InitializePluginSolution(InitializePluginSolutionOptions opts)
         {
             var outputDir = Directory.GetCurrentDirectory();
             var solution = new PluginSolution(outputDir, opts.Name, new GitClient(outputDir));
@@ -52,10 +47,10 @@ namespace BTCPayTool
                 .Tap(() => Log.Information("The plugin solution has been initialized. You can now add your first plugin by executing: btcpay new-plugin --name MyPlugin"))
                 .TapError(error => Log.Error("Plugin creation failed: {Error}", error));
 
-            return result.Match(() => 0, _ => -1);
+            result.Match(() => 0, _ => -1);
         }
 
-        private static async Task<int> NewPlugin(NewPluginOptions opts)
+        private static async Task NewPlugin(NewPluginOptions opts)
         {
             var outputDir = Directory.GetCurrentDirectory();
             var plugin = new Plugin(outputDir, opts.Name, new GitClient(outputDir));
@@ -65,7 +60,7 @@ namespace BTCPayTool
                 .Tap(pluginPath => Log.Information("The plugin has been added successfully! You can see it under {Path}", pluginPath))
                 .TapError(error => Log.Error("Plugin creation failed: {Error}", error));
 
-            return result.Match(_ => 0, _ => -1);
+            result.Match(_ => 0, _ => -1);
         }
     }
 }
